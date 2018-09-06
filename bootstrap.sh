021ada078f7e80261ed322e47314a561911d0a4e
Matte Edens <matte@silent-e.com>
2018-09-06 13:23:16 -0700

    delete .powconfig. allow preview of changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,13 @@ git pull origin master
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
     --exclude "laptop" --exclude "LICENSE-MIT.txt" \
+    -av --no-perms . ~
+	source ~/.bash_profile
+}
+
+function previewIt() {
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+    --exclude "laptop" --exclude "LICENSE-MIT.txt" \
     --dry-run \
     -av --no-perms . ~
 	source ~/.bash_profile
@@ -13,11 +20,15 @@ function doIt() {
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n/[p]review) " -n 1
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt
+  elif [[ $REPLY =~ ^[Pp]$ ]]; then
+    echo "Here's a preview..."
+    previewIt
 	fi
 fi
 
 unset doIt
+unset previewIt
